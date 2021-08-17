e06174d449abb5c857f3397b6b94b604c9b675b8
Shahriar Swim <shahriar2k18@gmail.com>
2021-08-17 23:38:32 +0600

    update skill test working

--- controller/skillTestController.js
+++ controller/skillTestController.js
@@ -55,8 +55,44 @@ export const createSkillTest = CatchAsync(async (req, res, next) => {
 
 // ------------------ update skill test -----------------
 export const updateSkillTest = CatchAsync(async (req, res, next) => {
+  const { questions, options, answers } = req.body;
+  const { tid } = req.params;
+
+  if (questions && options && answers) {
+    if (
+      Object.keys(questions).length <= 0 ||
+      Object.keys(options).length <= 0 ||
+      Object.keys(answers).length <= 0
+    ) {
+      return next(new AppError("No questions or options or answers.", 400));
+    }
+
+    if (
+      Object.keys(questions).length !== Object.keys(options).length ||
+      Object.keys(options).length !== Object.keys(answers).length ||
+      Object.keys(questions).length !== Object.keys(answers).length
+    ) {
+      return next(new AppError("question !== options !== answers", 400));
+    }
+  }
+
+  // is skill test available?
+  const isAvailable = await SkillTests.findById(tid);
+
+  if (!isAvailable) {
+    return next(new AppError("No skill test found with this id.", 404));
+  }
+
+  // update skill test
+  const skillTests = await SkillTests.findByIdAndUpdate(
+    { _id: tid },
+    { ...req.body },
+    { new: true }
+  );
+
   return res.status(200).json({
     status: "success",
+    skillTests,
   });
 });
 

6909be3c5b20139448bd314bdeb5b995fe563065
Shahriar Swim <shahriar2k18@gmail.com>
2021-08-24 16:18:35 +0600

    new message, get messages working

--- controller/chatController.js
+++ controller/chatController.js
@@ -40,10 +40,66 @@ export const createConversation = catchAsync(async (req, res, next) => {
   // get all the conversations
   const conversations = await Conversations.find({
     members: { $in: [senderId] },
-  });
+  }).sort("-createdAt");
 
   return res.status(201).json({
     status: "success",
     conversations,
+  });
+});
+
+// ----------------------- new message ------------------
+export const newMessage = catchAsync(async (req, res, next) => {
+  const { _id: senderId } = req.user;
+  const { cid } = req.params;
+  const { text } = req.body;
+
+  // check if conversatin exist
+  const isExist = await Conversations.findById(cid);
+  if (!isExist) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Wrong conversation id.",
+    });
+  }
+
+  // save new msg
+  const messages = new Messages({
+    conversationId: cid,
+    senderId,
+    text,
+  });
+  await messages.save();
+
+  return res.status(201).json({
+    status: "success",
+    messages,
+  });
+});
+
+// ------------------- get messages -------------------
+export const getMessages = catchAsync(async (req, res, next) => {
+  const { cid } = req.params;
+  const { _id: uid } = req.user;
+
+  // check if conversation exist
+  const isExist = await Conversations.findOne({
+    _id: cid,
+    members: { $in: [uid] },
+  });
+
+  if (!isExist) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Wrong conversation id or you are not in that conversation.",
+    });
+  }
+
+  // get all the messages of that cid
+  const messages = await Messages.find({ conversationId: cid });
+
+  return res.status(200).json({
+    status: "success",
+    messages,
   });
 });
--- routes/chatsRoutes.js
+++ routes/chatsRoutes.js
@@ -4,14 +4,22 @@ import { Router } from "express";
 import {
   createConversation,
   getConversations,
+  newMessage,
+  getMessages,
 } from "../controller/chatController.js";
 import { checkAuth } from "../middleware/checkAuth.js";
+import {
+  chatsValidateRules,
+  validate,
+} from "../middleware/validators/chatsValidator.js";
 
 const router = Router();
 
 // routes
 router.post("/conversations/:receiverId", checkAuth, createConversation);
 router.get("/conversations", checkAuth, getConversations);
+router.post("/:cid", chatsValidateRules(), validate, checkAuth, newMessage);
+router.get("/:cid", checkAuth, getMessages);
 
 // export
 export const chatsRoutes = router;
